a2b6ceb86821527b18c8bbc82063ca70b52f99db
meandmom <fraoltes@gmail.com>
2021-08-29 00:39:11 -0700

    To print size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,10 +9,10 @@ int main(void)
 	char a;
 	int b;
 	long int c;
-	long long init d;
+	long long int d;
 	float e;
 printf("size of a char: %zu byte(s)\n", sizeof(a));
-printf("size of an init: %zu byte(s)\n", sizeof(b));
+printf("size of an int: %zu byte(s)\n", sizeof(b));
 printf("size of a long int: %zu byte(s)\n", sizeof(c));
 printf("size of a long long int: %zu byte(s)\n", sizeof(d));
 printf("size of a float: %zu byte(s)\n", sizeof(e));
