266ef3185643b9eb53c1b22b7c3a0e374eacb0a3
meandmom <fraoltes@gmail.com>
2021-08-28 21:48:06 -0700

    Hot fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
-* main - Prints "Programming is like building a multilingual puzzle
+* main - Prints Programming is like building a multilingual puzzle
 *
-* return: return always zero
+* return: Return always zero
 */
 int main(void)
 {
