f13dd3e571e58dd6c3a6b9c0dc086d483efe6cc4
meandmom <fraoltes@gmail.com>
2021-08-28 19:13:13 -0700

    hot fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-/**
+/*
 *This function will print character using puts
 */
 int main(void)
 {
-/**
+/*
 *printing characters returning 0
 */
 puts("\"Programming is like building a multilingual puzzle");
