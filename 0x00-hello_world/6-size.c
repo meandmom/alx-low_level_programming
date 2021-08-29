9bc4c5c8b90daf44e44fc519c8e7ddcd72f74bae
meandmom <fraoltes@gmail.com>
2021-08-28 23:45:44 -0700

    To print size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+* main - Entery point
+*
+* Return: always 0
+*/
+int main(void)
+{
+	char chartype;
+	int inttype;
+	long int longinttype;
+	long long int longlonginttype;
+	float floattype;
+	printf("size of a char: %zu byte(s)\n", sizeof(chartype));
+	printf("size of an init: %zu byte(s)\n", sizeof(inttype));
+	printf("size of a long int: %zu byte(s)\n", sizeof(longinttype));
+	printf("size of a long long int: %zu byte(s)\n", sizeof(longlonginttype));
+
+}
