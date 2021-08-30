1c33a2bed06ab14ab2db856e702fe0220479f401
meandmom <fraoltes@gmail.com>
2021-08-30 12:30:42 -0700

    This will show the sine of nubmer

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -6,21 +6,18 @@
 *
 * Return: Always 0 (success)
 */
-/**
-* betty style doc goes there
-*/
 int main(void)
 {
-	int n;
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	printf(" ");
-	scanf("%d", &n);
-	if ( n > 0 )
-		printf("%d is positive", n);
-	else if ( n < 0 )
-		printf("%d is negative", n);
-	else if ( n == 0 )
-		printf("% is zero", n);
-	return (0);
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+printf(" \n");
+scanf("%d", &n);
+if (n > 0)
+printf("%d is positive\n", n);
+else if (n < 0)
+printf("%d is negative\n", n);
+else if (n == 0)
+printf("% is zero\n", n);
+return (0);
 }
