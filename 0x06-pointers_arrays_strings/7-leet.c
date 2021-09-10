65ea8698d5f7c924c679a4fb480e62f5aa343b25
fraol <fraoltes22@gmail.com>
2021-09-10 06:57:34 -0700

    Function incides a string into 1337

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+* leet-encode a string into 1337.
+* @s:pointer to the string to be encoded.
+*
+* Return: encoded string.(1337)
+*/
+char *leet(char *s)
+{
+int i = 0, j;
+char leet[5] = {'a', 'e', 'o', 't', 'l'};
+char leetencoded[5] = {'4', '3', '0', '7', '1'};
+for (i = 0; s[i] != '\0'; i++)
+{
+for (j = 0; j < 5; j++)
+{
+if (s[i] ==  leet[j] || s[i] ==(leet[j] - 32))
+{
+*(s + i) = leetencoded[j];}
+}
+}
+return (s);
+}
