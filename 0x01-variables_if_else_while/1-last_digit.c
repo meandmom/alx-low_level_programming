e39c551e1fd2d92a7e7a11ed589f244d7297c10a
fraol <fraoltes22@gmail.com>
2021-12-03 05:07:10 -0800

    Program that assigns random number and prints the last digig

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,23 @@
+#include <stdlib>
+#include <time.h>
+#include <stdio.h>
+/**
+* main - assign random number to int n
+*
+* Return: Always 0 ()
+*/
+int main(void)
+{
+int n;
+strad(time(0));
+n = rand() - RAND_MAX / 2;
+scanf("%d", &n);
+last = n % 10;
+if (last > 5)
+printf("Last digit of %d is %d and is greater than 5", last, n);
+else if (last = 0)
+printf("Last digit of %d is %d and is 0");
+else if
+printf("Last digit of %d is %d and less than 6 and not 0", last, n);
+return (0);
+}
