855219699e7cf13031243ca0233fed757384fbdd
fraol <fraoltes22@gmail.com>
2021-12-03 03:42:20 -0800

    The program that assign to random number add prints whether positive or negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -11,13 +11,12 @@ int main(void)
 int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-printf(" ");
 scanf("%d", &n);
 if (n > 0)
-printf("%dis positive\n", n);
+printf("%d is positive\n", n);
 else if (n < 0)
-printf("%dis negative\n", n);
+printf("%d is negative\n", n);
 else
-printf("%dis zero\n", n);
+printf("%d is zero\n", n);
 return (0);
 }
