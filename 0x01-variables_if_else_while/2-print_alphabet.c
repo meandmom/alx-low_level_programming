c1af7b291f1e56944f49abab90bc4f13437bb26e
fraol <fraoltes22@gmail.com>
2021-12-04 00:52:16 -0800

    Program that prints alphabet in lowercase followed by new line

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -4,10 +4,10 @@
 *
 * Return: Always 0 (success)
 */
-int main()
+int main(void)
 {
 char ch;
-for (ch = 'a';ch <= 'z';ch++)
+for (ch = 'a'; ch <= 'z'; ch++)
 putchar(ch);
 putchar('\n');
 return (0);
