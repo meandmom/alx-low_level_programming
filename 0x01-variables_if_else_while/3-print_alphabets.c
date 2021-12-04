253def0e760bc7e0cbba3594ac34c2bfe2654a35
fraol <fraoltes22@gmail.com>
2021-12-04 04:03:28 -0800

    Program that prints alphabets in lowercase and uppercase

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* main - intery point
+*
+* Return: Always 0(success)
+*/
+int main(void)
+{
+int ch = 'a';
+int CH = 'A';
+for (ch = 'a'; ch <= 'z'; ch++)
+putchar(ch);
+for (CH = 'A'; CH <= 'Z'; CH++ )
+putchar(CH);
+putchar('\n');
+return (0);
+}
