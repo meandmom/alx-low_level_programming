1d912f78e9fdc62a137760cd86543a7548cd8720
fraol <fraoltes22@gmail.com>
2021-12-04 05:05:29 -0800

    Program that prints alphabet in lowercase omiting e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+* main - intery point
+*
+* Return: Always 0(success)
+*/
+int main(void)
+{
+int ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+if(ch != 'e' && ch != 'q')
+putchar(ch);
+putchar('\n');
+return (0);
+}
