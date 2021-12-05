82bf4d0272e2acbb77f846c40885924e74d528d2
fraol <fraoltes22@gmail.com>
2021-12-05 00:05:33 -0800

    Program that prints alphabet in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+* main - Entery point
+*
+* Return: Always 0(success)
+*/
+int main(void)
+{
+int c;
+for (c = 'z'; c >= 'a'; c--)
+putchar(c);
+putchar('\n');
+return (0);
+}
