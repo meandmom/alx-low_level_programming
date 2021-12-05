0e81a10ec080f622f3788603abd8c54b2d1a3950
fraol <fraoltes22@gmail.com>
2021-12-04 23:48:57 -0800

    Program that prints all single digit numbers of base ten

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+* main - Entery point
+*
+* Return: Always 0(success)
+*/
+int main(void)
+{
+int n;
+for (n = '0'; n <= '9'; n++)
+putchar(n);
+putchar('\n');
+return (0);
+}
