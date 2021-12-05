b6d927fa406d3f56785fdc9adbdcdae9c73a5e20
fraol <fraoltes22@gmail.com>
2021-12-05 09:59:19 -0800

    Hot fix

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+* main - Entery point 
+*
+* Return: Always 0 (success)
+*/
+int main(void)
+{
+int n;
+for (n = 0; n <= 9; n++)
+{
+putchar((n % 10) + '0');
+if (n == 9)
+{
+continue;
+}
+putchar(',');
+putchar(' ');
+}
+putchar('$');
+return (0);
+}
